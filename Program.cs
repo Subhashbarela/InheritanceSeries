5bcb51ec90d0867d744a4534d1ac600f0505deaf
Subhash Barela <barelasubhash16@gmail.com>
2023-02-26 14:26:55 +0530

    create simple program using interface

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using OopsConcept;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    interface IPolygon
+    {
+         void calculateArea(int l, int b);
+
+    }
+    class Program: IPolygon
+    {
+        public void calculateArea(int l, int b)
+        {
+            
+            int area = l * b;
+            Console.WriteLine("Area of Rectangle: " + area);
+        }
+        static void Main(string[] args)
+            {
+          Program Obj= new Program();
+            Obj.calculateArea(1, 2);
+            
+           }
+        }
+    
+}
